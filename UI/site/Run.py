9950baa5f208717239c126e6e1bef1600412cd44
Zanice <znj@case.edu>
2017-03-16 20:23:31 -0400

    Created wrapping template, template inheritance for consistent site appearance

--- UI/site/Run.py
+++ UI/site/Run.py
@@ -8,33 +8,54 @@ app = Flask(__name__)
 
 # template dictionary
 TEMPLATE_DIC = {
-	'Home': ('home', '/'),
-	'Login': ('login', '/login')
+	'Test Page': ('test', '/test', 'PoM TEST PAGE'),
+	'Home': ('home', '/', 'Home - Presents of Mind'),
+	'Login': ('login', '/login', 'Login - Presents of Mind')
 }
+
+# template dictionary entry index constants
+TEMPLATE_DIC_NAME_ENTRY = 0
+TEMPLATE_DIC_PATH_ENTRY = 1
+TEMPLATE_DIC_PAGE_HEAD_ENTRY = 2
 
 def create_nav_bar():
 	nav_bar = []
 	
 	for name in TEMPLATE_DIC:
-		url = TEMPLATE_DIC[name][1]
-		print(url)
-		nav_bar.append((name, url))
-		print(nav_bar)
+		path = TEMPLATE_DIC[name][TEMPLATE_DIC_PATH_ENTRY]
+		nav_bar.append((name, path))
 	
 	return nav_bar
 
-@app.route(TEMPLATE_DIC['Home'][1])
+# EMPTY PAGE FOR TESTING
+@app.route(TEMPLATE_DIC['Test Page'][TEMPLATE_DIC_PATH_ENTRY])
+def test_page_template():
+	return render_template(
+		# template name, from dictionary
+		TEMPLATE_DIC['Test Page'][TEMPLATE_DIC_NAME_ENTRY],
+		
+		# common fields
+		nav_bar=create_nav_bar(),
+		current_nav='Test Page',
+		page_header=TEMPLATE_DIC['Test Page'][TEMPLATE_DIC_PAGE_HEAD_ENTRY],
+		
+		# template-specific fields
+		dummy=''
+	)
+
+@app.route(TEMPLATE_DIC['Home'][TEMPLATE_DIC_PATH_ENTRY])
 def home_template():
 	return render_template(
 		# template name, from dictionary
-		TEMPLATE_DIC['Home'][0],
+		TEMPLATE_DIC['Home'][TEMPLATE_DIC_NAME_ENTRY],
 		
 		# common fields
 		nav_bar=create_nav_bar(),
 		current_nav='Home',
+		page_header=TEMPLATE_DIC['Home'][TEMPLATE_DIC_PAGE_HEAD_ENTRY],
 		
 		# template-specific fields
-		page_header='Presents of Mind'
+		dummy=''
 	)
 
 if __name__ == '__main__':
