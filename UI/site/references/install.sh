66b1e6ef58d03fd65035a547f63ea0a281971d2f
Zanice <znj@case.edu>
2017-04-18 21:40:45 -0400

    Reverted (I think) JS Facebook integration in templates; install instructions for python Facebook API

--- UI/site/references/install.sh
+++ UI/site/references/install.sh
@@ -1,5 +1,10 @@
+sudo apt-get install postgresql
+sudo easy_install sqlalchemy
 sudo apt-get install python-pip
 sudo pip install Flask
+sudo pip install Flask-Social
+sudo pip install Flask-SQLAlchemy
+sudo pip install http://github.com/pythonforfacebook/facebook-sdk/tarball/master
 
 sudo pip install facebook-sdk
 
