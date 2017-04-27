1f343f7d3e90ea62f1fc7c29d212b0953cb4f9cd
Zanice <znj@case.edu>
2017-04-27 15:57:47 -0400

    More processing of Facebook post data, shipped as interests

--- UI/site/processes/facebook.py
+++ UI/site/processes/facebook.py
@@ -1,4 +1,5 @@
 # native imports
+import string
 
 # project imports
 
@@ -6,15 +7,25 @@
 
 
 SHARED_KEYWORD = ' shared '
-
 SHARED_ITEM_KEYWORDS = [
 	'\'s photo.',
 	'\'s video.'
 ]
 
+WHITESPACE_TO_REMOVE = string.whitespace.replace('\n', '').replace(' ', '')
+
+def sanitize(string):
+	ascii_string = ''
+	for char in string:
+		if 0 < ord(char) <= 255:
+			ascii_string += char
+	return str(ascii_string)
+
 def prune_story(name, story):
 	if story is None:
 		return story
+	
+	story = sanitize(story)
 	
 	shared_story_start = name + SHARED_KEYWORD
 	
@@ -33,30 +44,64 @@ def prune_story(name, story):
 	
 	return None
 
-def sanitize_message(message):
+def prune_message(message):
 	if message is None:
-		return message
+		return None
 	
-	# TODO: sanitize of annoying-ass emoji characters
+	message = sanitize(message)
 	
-	return message
+	message = message.strip(WHITESPACE_TO_REMOVE)
+	message = message.replace('\n', ' ')
+	message = message.replace('\\n', ' ')
+	
+	index = 0
+	left_index = -1
+	hashtags = []
+	while index < len(message):
+		current_char = message[index]
+		if left_index is -1:
+			if current_char == '#':
+				left_index = index
+		else:
+			current_char_code = ord(current_char)
+			is_number = 48 <= current_char_code <= 57
+			is_lc_letter = 97 <= current_char_code <=122
+			is_uc_letter = 65 <= current_char_code <= 90
+			if not (is_number or is_lc_letter or is_uc_letter):
+				hashtag = message[left_index + 1:index]
+				message = message[:left_index] + message[index:]
+				hashtags.append(hashtag)
+				index = left_index
+				left_index = -1
+		
+		index += 1
+	if left_index is not -1:
+		hashtag = message[left_index + 1:]
+		message = message[:left_index]
+		hashtags.append(hashtag)
+	
+	return (message, hashtags)
 
 def digest_posts(name, posts):
 	stories = []
 	messages = []
+	hashtags = []
 	
 	for post in posts:
 		current_story = prune_story(name, post['story'])
-		current_message = sanitize_message(post['message'])
+		current_message = prune_message(post['message'])
 		
 		if current_story is not None:
 			stories.append(current_story)
 		if current_message is not None:
-			messages.append(current_message)
+			messages.append(current_message[0])
+			for hashtag in current_message[1]:
+				hashtags.append(hashtag)
 	
 	digested_posts = {}
 	digested_posts['stories'] = stories
 	digested_posts['messages'] = messages
+	digested_posts['hashtags'] = hashtags
 	
 	return digested_posts
 
@@ -64,7 +109,17 @@ def info_from_json(json):
 	name = str(json['name'])
 	
 	digested_posts = digest_posts(name, json['posts'])
-	print(digested_posts['stories'])
+	#print("STORIES: {}".format(digested_posts['stories']))
+	#print("HASHTAGS: {}".format(digested_posts['hashtags']))
+	#print("MESSAGES: {}".format(digested_posts['messages']))
+	
+	# TODO: word frequency processing of messages for more interests
+	
+	interests = []
+	for story in digested_posts['stories']:
+		interests.append(story)
+	for hashtag in digested_posts['hashtags']:
+		interests.append(hashtags)
 	
 	info = {}
 	
@@ -72,6 +127,6 @@ def info_from_json(json):
 	info['age'] = None
 	info['gender'] = None
 	info['hometown'] = None
-	info['interests'] = None
+	info['interests'] = interests
 	
 	return info
