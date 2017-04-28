962ce92e0868f49322b2cd0c8eaa9cc155125393
Alex DeFiore <aad88@case.edu>
2017-04-27 21:01:17 -0400

    amazon search by id and keyword working

---
+++ UI/site/processes/amazon_search.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python
+from amazonproduct import API
+from amazonproduct import AWSError
+import bottlenose
+#from sys import argv
+
+api = API(locale='us')
+amazon = bottlenose.Amazon('AKIAIW3XL4AGXY7DFAQQ', 'pLlEYwzIpEdoygCvhp3KmpsotpkmZZwfpDZFa11t', 'chewie027-20')
+
+def searchByKeyword(keyword, minPrice = -1, maxPrice = -1):
+  items = None
+  try:
+    if (minPrice == -1):
+      items = api.item_search('All', Keywords=keyword, ResponseGroup='Medium')
+    else:
+      items = api.item_search('All', Keywords=keyword, MinimumPrice=minPrice, MaximumPrice=maxPrice, ResponseGroup='Medium')
+  except AWSError as e:
+    print 'Amazon complained about request'
+    print e.code
+    print e.msg
+    return None
+  results = []
+  count = 0
+  for result in items:
+    obj = {}
+    obj['id'] = result.ASIN
+    obj['name'] = result.ItemAttributes.Title
+    obj['url'] = result.DetailPageURL
+    try:
+      obj['price'] = result.ItemAttributes.ListPrice.FormattedPrice
+    except:
+      obj['price'] = None
+    obj['imageurl'] = result.SmallImage.URL
+    obj['imageheight'] = result.SmallImage.Height
+    obj['imagewidth'] = result.SmallImage.Width
+    results.append(obj)
+    count += 1
+    if count == 3:
+        break
+  return results
+
+def searchById(id):
+    items = None
+    try:
+        items = api.item_lookup(ItemId=id, ResponseGroup='Medium')
+    except AWSError as e:
+        print 'Amazon complained about request'
+        print e.code
+        print e.msg
+    results = []
+    result = items.Items.Item
+    obj = {}
+    obj['id'] = id
+    obj['name'] = result.ItemAttributes.Title
+    obj['url'] = result.DetailPageURL
+    try:
+      obj['price'] = result.ItemAttributes.ListPrice.FormattedPrice
+    except:
+      obj['price'] = None
+    obj['imageurl'] = result.SmallImage.URL
+    obj['imageheight'] = result.SmallImage.Height
+    obj['imagewidth'] = result.SmallImage.Width
+    results.append(obj)
+    return results
